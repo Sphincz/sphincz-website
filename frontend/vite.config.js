c7b4c361f0fd1c6da65384a3726f4e6e6a6bc6e9
António Raimundo <aslro@iscte-iul.pt>
2024-09-09 12:09:10 +0100

    SPW v0.0.4 ~ Fix Dockerfile write permissions + Enabled Go C compiler in Dockerfile + Added development environments to enable Hot-reload features

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,13 @@ import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+  },
   plugins: [
     vue(),
     Icons({
