120f0f7ae6d9a8dee0cabe262c078b6c3077fabf
António Raimundo <aslro@iscte-iul.pt>
2024-11-24 17:13:16 +0000

    feat: About me page done.

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,33 +1,36 @@
+<script setup>
+import { useI18n } from "vue-i18n"
+const { t } = useI18n()
+</script>
 <template>
 	<section class="pt-page">
 		<div class="container">
 			<div class="row mb-4">
 				<div class="col">
-					<h1>About</h1>
+					<h1>{{ t("about.title") }}</h1>
 				</div>
 			</div>
 			<div class="row mb-3">
 				<div class="col-auto me-auto text-start">
-					<div class="p-3 float-md-end d-block mx-auto" style="width: 190px">
-						<img class="img-fluid rounded-4" src="../assets/me.jpg" alt="Gabe's Avatar" />
+					<div class="m-2 mx-auto d-block mx-auto" style="width: 180px">
+						<img class="img-fluid rounded-circle" src="../assets/me.png" alt="Raimundo's Avatar" />
 					</div>
-					<p>Hi! I'm Gabe.</p>
+					<p>{{ t("about.subtitle") }}</p>
+					<p>{{ t("about.desc1") }}</p>
+					<p>{{ t("about.desc2") }}</p>
 					<p>
-						I'm a DevOps Engineer and Software Developer based in Oklahoma City, Oklahoma, USA. I'm
-						passionate about automating processes, crafting innovative solutions, and staying up-to-date
-						with the latest technologies. I specialize in building robust systems using Golang and Vue.js,
-						and I enjoy exploring new ideas and tinkering with self-hosted home infrastructure. My
-						<router-link to="/projects">project</router-link> page showcases some of my most notable works.
+						<i18n-t keypath="about.desc3">
+							<template #projectLink>
+								<router-link to="/projects">{{ t("navbar.projects") }}</router-link>
+							</template>
+						</i18n-t>
 					</p>
 					<p>
-						To ensure efficient development and deployment, I utilize containers, GitHub Actions,
-						Kubernetes, and GitOps. These tools allow me to streamline the development lifecycle and ensure
-						that my projects run seamlessly. If you're looking for a reliable, results-driven DevOps
-						Engineer and Software Developer, look no further! I'm always excited to collaborate on new
-						projects and leverage my skills to help organizations achieve their goals.
-					</p>
-					<p>
-						<router-link to="/contact">Feel free to reach out!</router-link>
+						<i18n-t keypath="about.desc4">
+							<template #contactMe>
+								<router-link to="/contact">{{ t("about.contactme") }}</router-link>
+							</template>
+						</i18n-t>
 					</p>
 				</div>
 			</div>
